73f8a9a062342f5028af05ca4a6262d0c7818ace
Marco Taborda <marcotaborda.jr@gmail.com>
2024-12-21 21:18:05 -0300

    feat: Consulta arquivo por id no Alfresco

--- src/arquivos/arquivos.controller.ts
+++ src/arquivos/arquivos.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Observable } from 'rxjs';
 import { Permissions } from 'src/authentication/decorators';
-import { ArquivosService } from './arquivos.service';
+import { Node } from 'src/config/alfresco/interfaces';
+import { AlfrescoNodeService } from 'src/config/alfresco/services';
 
 @ApiTags('Arquivos')
 @Controller('arquivos')
 export class ArquivosController {
-  constructor(private readonly service: ArquivosService) {}
+  constructor(private readonly service: AlfrescoNodeService) {}
 
-  @Get()
-  @Permissions('lista:arquivos')
-  @ApiOperation({ summary: 'Lista arquivos no repositório Alfresco ECM' })
-  lista() {
-    return this.service.lista();
+  @Get(':id')
+  @Permissions('consulta:arquivos')
+  @ApiOperation({ summary: 'Consulta arquivo no repositório Alfresco ECM' })
+  consulta(@Param('id') id: string): Observable<Node> {
+    return this.service.consulta(id);
   }
 }
--- src/arquivos/arquivos.module.ts
+++ src/arquivos/arquivos.module.ts
@@ -1,9 +1,23 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ArquivosService } from './arquivos.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AlfrescoInterceptor } from 'src/config/alfresco/interceptors';
+import {
+  AlfrescoAuthService,
+  AlfrescoNodeService,
+} from 'src/config/alfresco/services';
 import { ArquivosController } from './arquivos.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [ArquivosController],
-  providers: [ArquivosService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AlfrescoInterceptor,
+    },
+    AlfrescoAuthService,
+    AlfrescoNodeService,
+  ],
 })
 export class ArquivosModule {}
