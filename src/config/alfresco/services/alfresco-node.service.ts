cb9b3b37b9bc96467de7323e67da267c454d201b
Marco Taborda <marcotaborda.jr@gmail.com>
2024-12-22 08:42:23 -0300

    feat: Efetua download de arquivo do Alfresco

--- src/config/alfresco/services/alfresco-node.service.ts
+++ src/config/alfresco/services/alfresco-node.service.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { map, Observable } from 'rxjs';
+import { Readable } from 'stream';
 import { Node } from '../interfaces';
 
 @Injectable()
@@ -10,7 +11,24 @@ export class AlfrescoNodeService {
 
   consulta(id: string): Observable<Node> {
     return this.http
-      .get<Node>(`${process.env.ALFRESCO_SERVICES_URL}/nodes/${id}`)
+      .get<Node>(this.montaUrl(id))
       .pipe(map((response: AxiosResponse<Node>) => response.data));
+  }
+
+  download(id: string): Promise<Readable> {
+    return new Promise<Readable>((resolve) =>
+      this.http
+        .get<Readable>(`${this.montaUrl(id)}/content`, {
+          responseType: 'stream',
+        })
+        .subscribe({ next: ({ data }) => resolve(data) }),
+    );
+  }
+
+  private montaUrl(id?: string): string {
+    if (!id) {
+      return `${process.env.ALFRESCO_SERVICES_URL}/nodes`;
+    }
+    return `${process.env.ALFRESCO_SERVICES_URL}/nodes/${id}`;
   }
 }
